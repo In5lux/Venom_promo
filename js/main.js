0315e39953af10127d5a58a51f62161910267bc2
In5lux <nikos_almeryda@mail.ru>
2021-06-16 00:44:58 +0300

    Финальная верстка

---
+++ js/main.js
@@ -0,0 +1,12 @@
+﻿const menuButton = document.querySelector('.menu-button');
+const navMenu = document.querySelector('.nav-menu');
+const menuClose = document.querySelector('.menu-close');
+menuButton.addEventListener('click', () => {
+  navMenu.classList.add('menu-active');
+  menuClose.classList.add('menu-active');
+  menuClose.addEventListener('click', () => {
+    navMenu.classList.remove('menu-active');
+    menuClose.classList.remove('menu-active');
+  })
+
+})
